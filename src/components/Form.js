11ecc9e9f215c9f1c57630bb0e6c399193e698cf
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-19 15:18:10 -0500

    all form elements in place

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,10 @@ const SignUpForm = ({ values }) => {
   return (
     <div>
       <Form>
+      <label>
+          Name
+          <Field type="text" name="name" />
+        </label>
         <label>
           Username
           <Field type="text" name="username" />
@@ -35,23 +39,44 @@ const SignUpForm = ({ values }) => {
             <option value='dnd'>Dungeons and Dragons</option>
             <option value='starfinder'>Starfinder</option>
             <option value='generic'>Other</option>
-
-            
             </Field>
         </label>
+        <label>
+          Where did you Hear About Us?
+          <Field component="select" name="reach" >
+            <option>Choose One</option>
+            <option value='flgs'>Gaming Store</option>
+            <option value='wordofmouth'>A Friend</option>
+            <option value='forum'>A Forum</option>
+            <option value='other'>Other</option>
+            </Field>
+        </label>
+        <label>
+            I am at least 13 years of age
+            <Field type="checkbox" name='age' checked={values.age}/>
+        </label>
+        <label>
+            I have read and agree to the Terms of Service
+            <Field type="checkbox" name='tos' checked={values.tos}/>
+        </label>
+        <button>Join Now!</button>
       </Form>
     </div>
   );
 };
 
 export const FormikSignUpForm = withFormik({
-  mapPropsToValues({ username, email, password, usertype, ruleset }) {
+  mapPropsToValues({ name, username, email, password, usertype, ruleset, reach, age, tos }) {
     return {
+      name: name || "",
       username: username || "",
       email: email || "",
       password: password || "",
       usertype: usertype || "",
       ruleset: ruleset || "",
+      reach: reach || "",
+      age: age || false,
+      tos: tos || false
     };
   }
 })(SignUpForm);
