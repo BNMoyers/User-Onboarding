71aa8fbb1e3c459598ec80fb1ad7aa5b0783fb3c
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-19 14:58:07 -0500

    added more fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,15 +11,47 @@ const SignUpForm = ({ values }) => {
           Username
           <Field type="text" name="username" />
         </label>
+        <label>
+          Email
+          <Field type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <Field type="text" name="password" />
+        </label>
+        <label>
+          Account Type
+          <Field component="select" name="usertype" >
+            <option>Select an Account Type</option>
+            <option value='gm'>Game Master/ Dungeon Master</option>
+            <option value='player'>Player</option>
+            </Field>
+        </label>
+        <label>
+          Ruleset
+          <Field component="select" name="ruleset" >
+            <option>Select a Ruleset for Your Game</option>
+            <option value='pathfinder'>Pathfinder</option>
+            <option value='dnd'>Dungeons and Dragons</option>
+            <option value='starfinder'>Starfinder</option>
+            <option value='generic'>Other</option>
+
+            
+            </Field>
+        </label>
       </Form>
     </div>
   );
 };
 
 export const FormikSignUpForm = withFormik({
-  mapPropsToValues({ username }) {
+  mapPropsToValues({ username, email, password, usertype, ruleset }) {
     return {
-      username: username || ""
+      username: username || "",
+      email: email || "",
+      password: password || "",
+      usertype: usertype || "",
+      ruleset: ruleset || "",
     };
   }
 })(SignUpForm);
